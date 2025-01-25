3974ff05c9467a568678b9ee05c91e12e37eb738
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-01-25 14:24:37 +0000

    chore: improve DLQ recovery

--- src/infrastructure/ports/dlq/main.py
+++ src/infrastructure/ports/dlq/main.py
@@ -1,12 +1,33 @@
 import logging
 
+from pika.exceptions import AMQPConnectionError
+
 from src.infrastructure.ports.dlq.app import App
 from src.infrastructure.ports.dlq.dependencies import configure
 
 
-logging.basicConfig(level=logging.INFO)
-
-app = configure(App(logger=logging.getLogger(__name__)))
-
 if __name__ == "__main__":
-    app.run()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("DLQ-SUBSCRIBER")
+    logger.info("Starting DLQ subscriber. Press Ctrl+C to end the process.")
+
+    should_run = True
+    max_retries = 3
+    while should_run and max_retries > 0:
+        app = configure(App(logger=logger))
+        logger.info("Starting DLQ subscriber")
+        try:
+            app.run()
+        except KeyboardInterrupt:
+            logger.info("Stoping DLQ subscriber")
+            should_run = False
+        except AMQPConnectionError as exc:
+            logger.error(f"We have to restart the dlq subscriber app. AMQPConnectionError: {exc}")
+            max_retries -= 1
+        finally:
+            app.stop()
+
+    if max_retries == 0:
+        logger.error("We have reached the maximum retries. The dlq subscriber will stop.")
+
+    logger.info("DQL subscriber stopped")
