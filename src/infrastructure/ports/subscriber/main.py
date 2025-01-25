2248c73922168d2f92588e2564acfa199919d81f
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-01-25 15:51:29 +0000

    chore: improve subscriber resiliency

--- src/infrastructure/ports/subscriber/main.py
+++ src/infrastructure/ports/subscriber/main.py
@@ -1,13 +1,33 @@
 import logging
 
+from pika.exceptions import AMQPConnectionError
+
 from src.infrastructure.ports.subscriber.app import App
 from src.infrastructure.ports.subscriber.dependencies import configure
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-app = configure(App(logger=logger))
-app.register(lambda sp: sp.register_singleton(logging.Logger, lambda _: logger))
-
 if __name__ == "__main__":
-    app.run()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("SUBSCRIBER")
+
+    should_run = True
+    max_retries = 3
+
+    while should_run and max_retries > 0:
+        app = configure(App(logger=logger))
+        app.register(lambda sp: sp.register_singleton(logging.Logger, lambda _: logger))
+        try:
+            app.run()
+        except KeyboardInterrupt:
+            logger.info("Clossing because of KeyboardInterrupt")
+            should_run = False
+        except AMQPConnectionError as exc:
+            logger.error(f"We have to restart the subscriber app. AMQPConnectionError: {exc}")
+            max_retries -= 1
+        finally:
+            app.stop()
+
+    if max_retries == 0:
+        logger.error("We have reached the maximum retries. The subscriber will stop.")
+
+    logger.info("Subscriber stopped")
--- tests/unit/infrastructure/ports/subscriber/test_app.py
+++ tests/unit/infrastructure/ports/subscriber/test_app.py
@@ -1,5 +1,5 @@
 from logging import Logger
-from unittest.mock import Mock, call
+from unittest.mock import Mock
 
 from src.infrastructure.ports.subscriber.app import App
 from src.seedwork.infrastructure.events import EventsSubscriber
@@ -14,36 +14,3 @@ def test_main_should_start_listening():
     app.run()
 
     events_subscriber.start_listening.assert_called_once()
-    logger.info.assert_has_calls(
-        [
-            call("Starting app. Press Ctrl+C to end the process."),
-            call("Subscriber starts listening"),
-            call("Stopping subscriber"),
-            call("Subscriber stops listening"),
-            call("Subscriber is stopped"),
-            call("App closed"),
-        ]
-    )
-
-
-def test_main_should_stop_listening_on_keyboard_interrupt():
-    events_subscriber = Mock(EventsSubscriber)
-    logger = Mock(Logger)
-    app = App(logger=logger)
-    app.register(lambda sp: sp.register(EventsSubscriber, lambda _: events_subscriber))
-    events_subscriber.start_listening.side_effect = KeyboardInterrupt
-
-    app.run()
-
-    events_subscriber.stop_listening.assert_called_once()
-    logger.info.assert_has_calls(
-        [
-            call("Starting app. Press Ctrl+C to end the process."),
-            call("Subscriber starts listening"),
-            call("Clossing because of KeyboardInterrupt"),
-            call("Stopping subscriber"),
-            call("Subscriber stops listening"),
-            call("Subscriber is stopped"),
-            call("App closed"),
-        ]
-    )
