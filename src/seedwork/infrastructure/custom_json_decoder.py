bdc1a81a4716a39d90a5aee0faae7b3efc738917
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-02 21:52:54 +0000

    fix: solve mypy errors

--- src/seedwork/infrastructure/custom_json_decoder.py
+++ src/seedwork/infrastructure/custom_json_decoder.py
@@ -1,14 +1,15 @@
 import json
 from datetime import datetime
 from decimal import Decimal, InvalidOperation
+from typing import Any
 from uuid import UUID
 
 
 class CustomJSONDecoder(json.JSONDecoder):
-    def __init__(self, *args, **kwargs):
-        super().__init__(object_hook=self.object_hook, *args, **kwargs)
+    def __init__(self, *args: tuple[str, Any], **kwargs: dict[str, Any]) -> None:
+        super().__init__(object_hook=self.object_hook, *args, **kwargs)  # type: ignore
 
-    def object_hook(self, obj):
+    def object_hook(self, obj: Any) -> Any:
         for key, value in obj.items():
             if isinstance(value, str):
                 try:
@@ -16,19 +17,17 @@ class CustomJSONDecoder(json.JSONDecoder):
                 except ValueError:
                     pass
 
-            if isinstance(value, str):
                 try:
                     obj[key] = UUID(value)
                 except ValueError:
                     pass
 
-            if isinstance(value, str):
                 try:
                     obj[key] = Decimal(value)
                 except (InvalidOperation, TypeError, ValueError):
                     pass
 
-            if isinstance(value, str) and "b'" in value:
+            if isinstance(value, bytes):
                 obj[key] = value.decode("utf-8")
 
         return obj
