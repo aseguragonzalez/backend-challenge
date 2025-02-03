b7421ea0b057d71d3ef6e7fe51831781d00ac578
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-03 17:45:15 +0000

    fix: solve mypy errors

--- src/infrastructure/ports/reconciler/services/reconciliation_service.py
+++ src/infrastructure/ports/reconciler/services/reconciliation_service.py
@@ -1,4 +1,6 @@
+from collections.abc import Mapping, Sequence
 from datetime import datetime, timedelta, timezone
+from typing import Any
 
 from pymongo.collection import Collection
 
@@ -9,9 +11,9 @@ from src.seedwork.infrastructure.queues.producer import Producer
 class ReconciliationService:
     def __init__(
         self,
-        db_events_collection: Collection,
-        db_dlq_events_collection: Collection,
-        db_processed_collection: Collection,
+        db_events_collection: Collection[dict[str, Any]],
+        db_dlq_events_collection: Collection[dict[str, Any]],
+        db_processed_collection: Collection[dict[str, Any]],
         producer: Producer,
     ) -> None:
         self._db_events_collection = db_events_collection
@@ -21,7 +23,7 @@ class ReconciliationService:
 
     def execute(self, seconds_delayed: int) -> None:
         filter_date = datetime.now(timezone.utc) - timedelta(seconds=seconds_delayed)
-        pipeline = [
+        pipeline: Sequence[Mapping[str, Any]] = [
             {"$match": {"created_at": {"$lte": filter_date.isoformat()}}},
             {
                 "$lookup": {
@@ -49,5 +51,6 @@ class ReconciliationService:
 
         [self._publish_event(document) for document in self._db_events_collection.aggregate(pipeline)]
 
-    def _publish_event(self, document: dict) -> None:
+    def _publish_event(self, document: dict[str, Any]) -> dict[str, Any]:
         self._producer.send_message(Event.from_dict(document).to_integration_event().to_bytes())
+        return document
